e960ba79fadf81a7eed6e91cef49a2b8c72d573e
JRHonda <justinohonda@gmail.com>
2018-04-23 16:10:28 -0400

    Initial commit

---
+++ mobile/src/main/java/com/rice_paddy/navifit/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.rice_paddy.navifit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.LogPrinter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = "Justin Hondda"
+        Log.d("myTag", "This is my message");
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
