0b1035f05c67b4c91c21057de4908688be9ead73
JRHonda <justinohonda@gmail.com>
2018-04-24 15:06:43 -0400

    commit new functions

--- mobile/src/main/java/com/rice_paddy/navifit/MainActivity.kt
+++ mobile/src/main/java/com/rice_paddy/navifit/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Button
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get button
-         Button btn = (Button) findViewById (R.id.testButton);
+        clickedButton(test(69))
+
 
 
         // Example of a call to a native method
@@ -22,12 +23,38 @@ class MainActivity : AppCompatActivity() {
         Log.d("myTag", "This is my message");
     }
 
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+         //Function that changes the text of the button when button is clicked
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    
 
+    fun test(number: Int): String {
+        return number.toString()
+    }
+
+    fun clickedButton(message: String) {
+        val clkBtn = findViewById(R.id.testButton) as Button
+
+        clkBtn.text = "Click me"
+
+        clkBtn.setOnClickListener {
+            testButton.text = message
+            Log.d("myTag", "You have clicked me");
+        }
+    }
 
     companion object {
 
